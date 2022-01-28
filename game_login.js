4ace9791abd22b3804a54d938d8d88594a903561
AnayMahajan <86356050+AnayMahajan@users.noreply.github.com>
2022-01-28 19:11:45 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function next_page(){
+    player1_name= document.getElementById("player_1_input").value;
+    player2_name= document.getElementById("player_2_input").value;
+
+    localStorage.setItem("player_1_input", player1_name);
+    localStorage.setItem("player_2_input", player2_name);
+
+    window.location= "game_page.html";
+}
